f4ccf41f439dc8317aa5ecaee439fedc21734c38
Jacob <jacobwbennett@gmail.com>
2022-11-24 01:57:12 -0700

    Add Jonah theme

--- public/background.js
+++ public/background.js
@@ -1,7 +1,9 @@
 chrome.runtime.onStartup.addListener(() => {
   chrome.storage.local.get(["theme"], obj => {
     if (obj.theme && obj.theme === "alt") {
-      chrome.browserAction.setIcon({ path: "icon48alt.png" });
+      chrome.action.setIcon({
+        path: "icon16alt.png"
+      });
     }
   });
 });
